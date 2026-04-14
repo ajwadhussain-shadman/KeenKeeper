5d9d158900ff1abfea6faa2defd2c2d01cb1c680
ajwadhussain-shadman <ajwadhussain38540@gmail.com>
2026-04-14 17:51:08 +0600

    banner section done

--- src/components/HomePage/Banner.jsx
+++ src/components/HomePage/Banner.jsx
@@ -3,12 +3,33 @@ import { FaPlus } from 'react-icons/fa';
 
 const Banner = () => {
     return (
-        <div className='text-center space-y-4 py-10'>
+        <div className='text-center space-y-4 py-10 md:w-3/4 w-11/12 mx-auto'>
             <h2 className='text-[#1F2937] font-bold md:text-5xl text-3xl'>Friends to keep close in your life</h2>
             <p className='text-[#64748B] '>
                 Your personal shelf of meaningful connections. Browse, tend, and nurture the <br />relationships that matter most.
             </p>
             <button className='btn mt-4 bg-[#244D3F] text-white font-semibold'><FaPlus />Add a Friend</button>
+        {/* Summery Card */}
+              <div className='grid grid-cols-2 md:grid-cols-4 justify-between my-6 gap-6 '>
+                <div className='p-8 text-center space-y-2 shadow rounded-lg bg-white'>
+                    <p className='font-semibold text-3xl text-[#244D3F]'>10</p>
+                    <p  className='text-[#64748B]'>Total Friends</p>
+                </div>
+                <div className='p-8 text-center space-y-2 shadow rounded-lg bg-white'>
+                    <p className='font-semibold text-3xl text-[#244D3F]'>10</p>
+                    <p  className='text-[#64748B]'>Total Friends</p>
+                </div>
+                <div className='p-8 text-center space-y-2 shadow rounded-lg bg-white'>
+                    <p className='font-semibold text-3xl text-[#244D3F]'>10</p>
+                    <p  className='text-[#64748B]'>Total Friends</p>
+                </div>
+                <div className='p-8 text-center space-y-2 shadow rounded-lg bg-white'>
+                    <p className='font-semibold text-3xl text-[#244D3F]'>10</p>
+                    <p  className='text-[#64748B]'>Total Friends</p>
+                </div>
+                
+              </div>
+
         </div>
     );
 };
--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -3,7 +3,7 @@ import Banner from './banner';
 
 const HomePage = () => {
     return (
-        <div>
+        <div className='bg-[#F8FAFC]'>
        <Banner></Banner>
         </div>
     );
